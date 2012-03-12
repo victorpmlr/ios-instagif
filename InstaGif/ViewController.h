ba5a50bf608311853ec6ea5b470eb5a5c3b7e745
PAMELARD Victor <vpamelard@P-215-18.intra.gobelins.fr>
2012-03-12 14:37:23 +0100

    Initial Commit

---
+++ InstaGif/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  InstaGif
+//
+//  Created by Eleve on 12/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
